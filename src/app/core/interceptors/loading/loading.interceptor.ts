c2f4466cc60574894482a413e1e5448526956767
mostafasabrry <mustafasabrri@gmail.com>
2025-07-07 02:46:44 +0300

    commit three

--- src/app/core/interceptors/loading/loading.interceptor.ts
+++ src/app/core/interceptors/loading/loading.interceptor.ts
@@ -1,13 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import {  NgxSpinnerService } from 'ngx-spinner';
+import { url } from 'node:inspector';
 import { finalize } from 'rxjs';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+   const ngxSpinnerService=inject(NgxSpinnerService);
+  if(req.url.includes('home') || req.url.includes('products') || req.url.includes('categories') || req.url.includes('cart') || req.url.includes('details'),req.url.includes('brands') || req.url.includes('wishlist') || req.url.includes('checkout') ) {
+   ngxSpinnerService.show();
+  }
 
-  const ngxSpinnerService=inject(NgxSpinnerService);
+ 
 
-  ngxSpinnerService.show();
+  
   return next(req).pipe( finalize(()=>{
     ngxSpinnerService.hide();
   }) )
